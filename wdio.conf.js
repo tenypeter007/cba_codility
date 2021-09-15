59c0b9b3df8d6c9a9a5488145cbaca2a16d7a2f5
teny <teny.peter007@gmail.com>
2021-09-15 23:08:11 +1000

    added junit report

--- wdio.conf.js
+++ wdio.conf.js
@@ -25,7 +25,15 @@ exports.config = {
         disableWebdriverStepsReporting: true,
         disableWebdriverScreenshotsReporting: true,
         useCucumberStepReporter : true
-    }]],
+    }],
+    ['junit', {
+        outputDir: './junit-results',
+        outputFileFormat: function(options) { // optional
+            return `results-${options.cid}.${options.capabilities}.xml`
+        }
+    }]
+
+],
 
     afterStep: function (test, scenario, { error, duration, passed }) {
         if (error) {
@@ -75,11 +83,14 @@ exports.config = {
         maxInstances: 5,
         //
         browserName: 'chrome',
-        acceptInsecureCerts: true
+        acceptInsecureCerts: true,
         // If outputDir is provided WebdriverIO can capture driver session logs
         // it is possible to configure which logTypes to include/exclude.
         // excludeDriverLogs: ['*'], // pass '*' to exclude all driver session logs
         // excludeDriverLogs: ['bugreport', 'server'],
+        'goog:chromeOptions': {
+            args: ['--headless', '--no-sandbox', '--disable-gpu', '--disable-dev-shm-usage --window-size=1920,1080', '--disable-translate', '--disable-extensions', '--disable-background-networking', '--disable-sync', '--disable-default-apps', '--mute-audio', '--no-first-run', '--disable-prompt-on-repost']
+        }
     }],
     //
     // ===================
